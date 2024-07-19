9c16868d99441688e69b0dcd7d56a9a70125b4ec
OlgaKobeleva <kobeleva.olga31@yandex.ru>
2024-07-19 22:28:49 +0300

    HW2
    Выполнила задание в двух вариантах. Второй вариант с помощью форматного вывода, как показывал Алексей на вебинаре.

--- homework2.py
+++ homework2.py
@@ -1,1 +1,7 @@
+number_of_tasks = 12
+hours = 1.5
+name ='Python'
+clock_of_tasks = hours / number_of_tasks
+print('Курс:', name, ', всего задач:', number_of_tasks, ', затрачено часов:', hours, ', среднее время выполнения ', clock_of_tasks, ' часа' '.', sep='')
 
+print(f"Курс:{name}, всего задач:{number_of_tasks}, затрачено часов:{hours}, среднее время выполнения {clock_of_tasks} часа.")
