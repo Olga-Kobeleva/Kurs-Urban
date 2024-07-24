13c238a4c59f2b637abac614eaeee7fccd951cbb
OlgaKobeleva <kobeleva.olga31@yandex.ru>
2024-07-24 20:41:24 +0300

    HW4

--- homework4.py
+++ homework4.py
@@ -1,1 +1,14 @@
+name = input('Введите ваше ФИО: ')   #input - произвольный текст
+num = int(input("Сколько вам лет? "))
+str_num = str(num)   #преобразование в строку
+length = len(str_num)  #len - кол-во символов в введеном тексте
+print('Здравствуйте,', name)
+print(length)
+
+print(name.upper())
+print(name.lower())
+print(name.replace(' ', ''))
+print(name [0])
+print(name [-1])
+
 
