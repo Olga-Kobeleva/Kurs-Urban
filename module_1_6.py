4f59c823c9513bbac236c5e8d747653eb6aaa86e
OlgaKobeleva <kobeleva.olga31@yandex.ru>
2024-07-30 21:46:17 +0300

    M1_6

--- module_1_6.py
+++ module_1_6.py
@@ -1,1 +1,17 @@
+my_dict = {'Denis' : 1986, 'Olga' : 1987, 'Tat`yana' : 1963}
+print(my_dict)
+print(my_dict['Denis'])
+my_dict['Artyom'] = 2000
+print(my_dict)
+my_dict.update({'Viktor' : 2003,
+                'Andrey' : 1989})
+a = my_dict.pop('Olga')
+print(a)
+print(my_dict)
 
+
+my_set = {1, 2 ,2, 'Apple', 'Apple', 3.54}
+print(my_set)
+my_set.update([13, 77])
+my_set.remove(2)
+print(my_set)
